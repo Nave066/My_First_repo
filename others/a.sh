98fd4d3c8ea589a77391e408e20304a52a582e08
Naveen V <94993935+Nave066@users.noreply.github.com>
2021-12-06 09:47:40 +0530

    Delete others directory
    It is not needed at present.

--- others/a.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in `ls *.txt`
-do
-fol=`echo $file | awk -F. '{print $1}'`;
-echo $fol;
-done
--- others/array.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-arr=(naveen 18597 kavin guhan 31200)
-declare -A array;
-echo ${arr[@]}
-echo ${arr[*]}
-echo ${arr[@]:1:4}
-echo ${arr[4]:1:3}
